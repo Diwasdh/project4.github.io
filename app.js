f93bac4112bb77e1b41d0c1ecc72a2ed08ae3744
diwasdh <diwasdhakal75@gmail.com>
2021-03-10 04:22:30 +0545

    10/03 star 4:22

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ container.onclick = e =>{
 };
 
 {
-    const container = document.querySelector('.rating2');
-const item=container.querySelectorAll(".star");
+    const container1 = document.querySelector('.rating2');
+const item1=container1.querySelectorAll(".star1");
 
-container.onclick = e =>{
+container1.onclick = e =>{
     const elclass=e.target.classList;
     if(!elclass.contains('active')){
-        item.forEach(
-            item=>item.classList.remove('active')
+        item1.forEach(
+            item1=>item1.classList.remove('active')
         );
         console.log(e.target.getAttribute("data-rate"));
-        console.log(container.getAttribute("id"));
+        console.log(container1.getAttribute("id"));
         elclass.add('active');
     }
 };
@@ -40,34 +40,34 @@ container.onclick = e =>{
 }
 
 {
-    const container = document.querySelector('.rating3');
-const item=container.querySelectorAll(".star");
+    const container2 = document.querySelector('.rating3');
+const item2=container2.querySelectorAll(".star2");
 
-container.onclick = e =>{
+container2.onclick = e =>{
     const elclass=e.target.classList;
     if(!elclass.contains('active')){
-        item.forEach(
-            item=>item.classList.remove('active')
+        item2.forEach(
+            item2=>item2.classList.remove('active')
         );
         console.log(e.target.getAttribute("data-rate"));
-        console.log(container.getAttribute("id"));
+        console.log(container2.getAttribute("id"));
         elclass.add('active');
     }
 };
 }
 
 {
-    const container = document.querySelector('.rating4');
-    const item=container.querySelectorAll(".star");
+    const container3 = document.querySelector('.rating4');
+    const item3=container3.querySelectorAll(".star3");
     
-    container.onclick = e =>{
+    container3.onclick = e =>{
         const elclass=e.target.classList;
         if(!elclass.contains('active')){
-            item.forEach(
-                item=>item.classList.remove('active')
+            item3.forEach(
+                item3=>item3.classList.remove('active')
             );
             console.log(e.target.getAttribute("data-rate"));
-            console.log(container.getAttribute("id"));
+            console.log(container3.getAttribute("id"));
             elclass.add('active');
         }
     };
